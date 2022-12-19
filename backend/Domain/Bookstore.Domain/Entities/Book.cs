af1d4607dce2a4fbdf327567e64273d428523a5e
Wisarut Chaloemanukulwattana <wisarutkim012@gmail.com>
2022-12-19 23:57:11 +0700

    initai backend

---
+++ backend/Domain/Bookstore.Domain/Entities/Book.cs
@@ -0,0 +1,13 @@
+﻿using Bookstore.Domain.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Domain.Entities
+{
+    public class Book : Entity<long>
+    {
+    }
+}
---
+++ backend/Domain/Bookstore.Domain/Entities/Interfaces/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookstore.Domain.Entities.Interfaces
+{
+    public abstract class Entity<T> : IBaseEntity<T>
+    {
+        public virtual T Id { get; protected set; }
+        T IBaseEntity<T>.Id
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+    }
+}
